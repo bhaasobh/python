39141691a97c6eb4d81c6f59b848834d8b57d674
Bahaa sobh <bhaasobh47@gmail.com>
2024-01-16 06:33:02 +0200

    init

--- ex1/data_summary.py
+++ ex1/data_summary.py
@@ -1,1 +1,31 @@
-print("bahahfafsnobh")
+import csv ,os,json
+
+class DataSummary:
+    def __init__(self,datafile: str,metafile: str):
+        self.datafile = datafile
+        self.metafile = metafile
+        self.features = []
+        self.data = dict()
+        with (open(self.metafile) as file):
+            reader = csv.reader(file)
+            for row in reader:
+                self.features.append(row)
+                break
+        print(len(self.features[0]))
+        for item in self.features[0]:
+            self.data = item
+
+        countries = []
+        with open(metafile , 'r') as file:
+            csv_dict = csv.DictReader(file)
+            for row in csv_dict:
+                countries.append(dict(row))
+                print(dict(row))
+
+        with open(self.datafile) as file:
+            data = json.load(file)
+            #print(data)
+        for record in data['data']:
+            print('***************')
+            for key,value in record.items():
+                print(key)
--- ex1/test.py
+++ ex1/test.py
@@ -9,34 +9,34 @@ if __name__ == "__main__":
         print("unexpected DataSummary constructor")
 
     DS = DataSummary(datafile="happiness.json",metafile="happiness_meta.csv")
-    print(DS[3])
-    print(DS["Country"])
-
-    try:
-        DS['GDP']
-    except Exception as err:
-        print("Exception: ", err)
-    else:
-        print("unexpected feature GDP")
-
-    try:
-        DS['data']
-    except Exception as err:
-        print("Exception: ", err)
-    else:
-        print("unexpected feature data")
-
-    print(DS.mean("Happiness Score"))
-    print(DS.mode("Class"))
-    print(DS.unique("Region"))
-
-    try:
-        DS.min("Country")
-    except Exception as err:
-        print("Exception: ", err)
-    else:
-        print("unexpected function min for categorical feature")
-
-
-    DS.to_csv("happiness.csv")
+    # print(DS[3])
+    # print(DS["Country"])
+    #
+    # try:
+    #     DS['GDP']
+    # except Exception as err:
+    #     print("Exception: ", err)
+    # else:
+    #     print("unexpected feature GDP")
+    #
+    # try:
+    #     DS['data']
+    # except Exception as err:
+    #     print("Exception: ", err)
+    # else:
+    #     print("unexpected feature data")
+    #
+    # print(DS.mean("Happiness Score"))
+    # print(DS.mode("Class"))
+    # print(DS.unique("Region"))
+    #
+    # try:
+    #     DS.min("Country")
+    # except Exception as err:
+    #     print("Exception: ", err)
+    # else:
+    #     print("unexpected function min for categorical feature")
+    #
+    #
+    # DS.to_csv("happiness.csv")
 
