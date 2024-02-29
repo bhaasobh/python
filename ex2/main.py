caac8dd82323092679d02d4cacb29c5421fd52ad
Bahaa sobh <bhaasobh47@gmail.com>
2024-02-29 14:46:21 +0200

    finish taslk 2

--- ex2/main.py
+++ ex2/main.py
@@ -58,8 +58,7 @@ phone_with_nan_Value = data[(data['f_camera'].isna()) & (data['camera'].isna())]
 print("Number of phones do not have a camera at all:", len(phone_with_nan_Value))
 
 #2
-# @title 1. How many phones do not have a camera at all (front or back)?
-# Filter the DataFrame based of who have single sim and have a camera with more than 12 pixel
+# @title 2.What is the average battery power for single-sim phones that have a camera or front camera  with a higher resolution than 12 megapixels?
 filter = data[(data['sim'] == 'Single') & ((data['camera'] > 12) | (data['f_camera'] > 12))]
 battery_power_AVG = filter['battery_power'].mean()
 print("Average battery power of phones whos have a single sim and have a camera with more than 12 pixel", battery_power_AVG)
@@ -81,4 +80,22 @@ else:
     print("No phone  that has no wifi, a touch screen and  weighs more than 145 grams")
 
 #4
-# @title
+# @title Create a pivot table that shows the percentage of phones with Bluetooth per generation,  pivoted around the phone generation and split by “ram” quartiles. (i.e. the rows are the  generation number and the columns are 4 quartiles of ram size).
+data['ram_quartile'] = pd.qcut(data['ram'], q=4)
+pivot_table = pd.pivot_table(data, values='bluetooth', index='gen', columns='ram_quartile', aggfunc=lambda x: np.mean(x == 'Yes'))
+print("Pivot table for Bluetooth percentage per generation and RAM quartiles:", pivot_table)
+
+#5
+# @title Create a new Dataframe based on the original that has the following features: [id,  battery_power, ram, talk_time, Bluetooth, cores, sim, memory, price], and contains a random  sampling of half of the medium speed phones.
+
+phones = data[data['speed'] == 'medium'].sample(frac=0.5, random_state=42)
+new_data = phones[['id', 'battery_power', 'ram', 'talk_time', 'bluetooth', 'cores', 'sim', 'memory', 'price']]
+print('new dataFrame for task 2,5',new_data)
+
+#6
+# @title Using this new dataset, what is the maximum total talk time you can achieve if you use 3  phones, and which 3 phones will you use?
+selected_phones = new_data.sort_values(by='talk_time', ascending=False).head(3)
+talk_time_maximum = selected_phones['talk_time'].sum()
+print(" phones with the maximun talk time:")
+print(selected_phones)
+print("\nMaximum total talk time achievable with these phones:", talk_time_maximum)
